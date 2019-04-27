a50179fd395511af04497838e3cf18fc440f847c
Carrosen <carla1.rosen@gmail.com>
2019-04-27 14:47:43 +0200

    sets up dafault for URL options in each enviroment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'user/index'
   root controller: :user, action: :index
 end
 
-# Rails.application.routes.draw do
+# # Rails.application.routes.draw do
+#   devise_for :users
   # get 'user/index'
 #   root controller: :articles, action: :index
 # end
