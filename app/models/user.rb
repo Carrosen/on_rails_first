f13d0a40817041a93aa5c99c2d302c9ee07e5344
Carrosen <carla1.rosen@gmail.com>
2019-04-28 14:05:13 +0200

    adding validations in User, all tests going green

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable   
+         :recoverable, :rememberable, :validatable 
+         
+  validates_presence_of :email, :encrypted_password
+  validates_uniqueness_of :email
+
+  
 end
